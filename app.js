9436028628b6c76f0636b93bfd7b0e7a088212e9
xNkee <133472902+xNkee@users.noreply.github.com>
2023-05-13 14:48:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -51,69 +51,6 @@ app.post('/preferencial', async (req, res) => {
   }
 });
 
-// Rota para buscar todas as senhas
-app.get('/senhas', async (req, res) => {
-  try {
-    const senhas = await Senhas.findAll({ order: [['id', 'DESC']] });
-
-    res.status(200).json({ senhas });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Erro ao buscar senhas' });
-  }
-});
-
-// Rota para atualizar o status de uma senha
-app.put('/senhas/:id/status', function(req, res) {
-  const id = req.params.id;
-  const { status, dataHoraStatus } = req.body;
-
-  // Verifica se o id é válido
-  if (isNaN(id)) {
-    return res.status(400).json({ message: 'ID inválido.' });
-  }
-
-  // Busca a senha no banco de dados
-  connection.query('SELECT * FROM Senhas WHERE id = ?', id, function(error, results, fields) {
-    if (error) {
-      return res.status(500).json({ message: 'Erro ao buscar a senha.', error: error });
-    }
-
-    // Verifica se a senha existe
-    if (results.length === 0) {
-      return res.status(404).json({ message: 'Senha não encontrada.' });
-    }
-
-    // Atualiza o status da senha
-    connection.query('UPDATE Senhas SET status = ?, data_hora_status = ? WHERE id = ?', [status, dataHoraStatus, id], function(error, results, fields) {
-      if (error) {
-        return res.status(500).json({ message: 'Erro ao atualizar o status da senha.', error: error });
-      }
-
-      // Calcula o tempo de permanência
-      const dataHoraEmissao = results[0].data_hora_emissao;
-      const tempoPermanencia = (new Date(dataHoraStatus).getTime() - new Date(dataHoraEmissao).getTime()) / 1000; // Em segundos
-
-      // Atualiza o tempo de permanência da senha
-      connection.query('UPDATE Senhas SET permanencia = ? WHERE id = ?', [tempoPermanencia, id], function(error, results, fields) {
-        if (error) {
-          return res.status(500).json({ message: 'Erro ao atualizar o tempo de permanência da senha.', error: error });
-        }
-
-        // Retorna a senha atualizada
-        connection.query('SELECT * FROM Senhas WHERE id = ?', id, function(error, results, fields) {
-          if (error) {
-            return res.status(500).json({ message: 'Erro ao buscar a senha atualizada.', error: error });
-          }
-          const senha = results[0];
-          return res.status(200).json(senha);
-        });
-      });
-    });
-  });
-});
-
-
 // Inicia o servidor na porta 3000
 app.listen(5000, () => {
   console.log('Servidor iniciado na porta 5000');
