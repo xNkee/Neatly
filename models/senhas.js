88b802f1e6a9f63faa568b1d5614a373c8027ae2
xNkee <133472902+xNkee@users.noreply.github.com>
2023-05-16 23:05:18 -0300

    Add files via upload

--- models/senhas.js
+++ models/senhas.js
@@ -2,8 +2,17 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('neatlydb', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false
 });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados bem sucedida.');
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar com o banco de dados:', err);
+  });
 
 const Senhas = sequelize.define('Senha', {
   id: {
